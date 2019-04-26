49b2d1d0ee2b3ce9b5529fadbb690383243fe68a
Anthony Rurup <tcrurup@gmail.com>
2019-04-26 03:16:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+line = ['Tony', 'Cody']
 def line(deli_line) 
   current_line = "The line is currently: "
   deli_line.each_with_index {|name, index| current_line += "#{index + 1} #{name}."}
