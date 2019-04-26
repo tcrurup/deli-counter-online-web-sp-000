7c8d505312523034d196119028edf0f57d0a4054
Anthony Rurup <tcrurup@gmail.com>
2019-04-26 03:21:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,12 @@ require 'pry'
 
 
 def line(deli_line) 
-  current_line = "The line is currently: "
-  deli_line.each_with_index {|name, index| current_line += "#{index + 1}. #{name} "}
-  current_line
-  binding.pry
+  if deli_line.length > 1
+    current_line = "The line is currently: "
+    deli_line.each_with_index {|name, index| current_line += "#{index + 1}. #{name} "}
+    current_line
+  else
+    deli_line
+  end
 end
 
-pline = ['Tony', 'Cody']
-line(pline)
