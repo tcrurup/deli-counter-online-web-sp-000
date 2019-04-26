a28cb0b79c0b03ea6bc5ee00154f5ffc73274c94
Anthony Rurup <tcrurup@gmail.com>
2019-04-26 03:26:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,3 @@
-require 'pry'
-
-
-
 def line(deli_line) 
   if deli_line.length >= 1
     current_line = "The line is currently:"
@@ -10,5 +6,10 @@ def line(deli_line)
   else
     puts "The line is currently empty."
   end
+end
+
+def take_a_number(current_line, name)
+  current_line << name
+  puts "Welcome #{name}. You are number #{current_line.length} in line."
 end
 
