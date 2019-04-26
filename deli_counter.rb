3bfea2b04583728d7de1cc206e2d8941b8fe310e
Anthony Rurup <tcrurup@gmail.com>
2019-04-26 03:23:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line(deli_line)
   if deli_line.length >= 1
     current_line = "The line is currently: "
     deli_line.each_with_index {|name, index| current_line += "#{index + 1}. #{name} "}
-    current_line
+    puts current_line
   else
-    "The line is currently empty."
+    puts "The line is currently empty."
   end
 end
 
