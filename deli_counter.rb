d26016398700e5ed8e27dbae4dc0c1b7cce23b0d
Anthony Rurup <tcrurup@gmail.com>
2019-04-26 03:18:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def line(deli_line) 
   current_line = "The line is currently: "
-  deli_line.each_with_index {|name, index| current_line += "#{index + 1} #{name}."}
+  deli_line.each_with_index {|name, index| current_line += "#{index + 1}. #{name} "}
   current_line
   binding.pry
 end
